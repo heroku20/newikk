8b38cd7e53b8494704e42e456989770a62307f49
heroku20 <130670798+heroku20@users.noreply.github.com>
2023-05-16 14:25:38 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,179 @@
+ <?php
+echo '
+<!DOCTYPE html>
+<html  >
+<head>
+  <!-- Site made with Mobirise Website Builder v5.8.9, https://mobirise.com -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v5.8.9, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="assets/images/logo5.png" type="image/x-icon">
+  <meta name="description" content="">
+  
+  
+  <title>Ikaria™️ Lean Belly Juice Reviews</title>
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons2/mobirise2.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/socicon/css/styles.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="preload" href="https://fonts.googleapis.com/css?family=Jost:100,200,300,400,500,600,700,800,900,100i,200i,300i,400i,500i,600i,700i,800i,900i&display=swap" as="style" onload="this.onload=null;this.rel=\'stylesheet\'">
+  <noscript><link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Jost:100,200,300,400,500,600,700,800,900,100i,200i,300i,400i,500i,600i,700i,800i,900i&display=swap"></noscript>
+  <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css"><link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+
+  
+  
+  
+</head>
+<body>
+  
+  <section data-bs-version="5.1" class="header11 cid-tDyo9gblld" id="header11-o">
+
+    
+
+    
+    
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-6 image-wrapper">
+                <a href="https://cutt.ly/officialikariajuice"><img class="w-100" src="assets/images/whatsapp-image-2023-05-07-at-8.20.44-pm-removebg-preview-614x407.png" alt="Ikaria Juice"></a>
+            </div>
+            <div class="col-12 col-md">
+                <div class="text-wrapper text-center">
+                    <h1 class="mbr-section-title mbr-fonts-style mb-3 display-2"><strong><em><u>Ikaria Lean Belly Juice™️&nbsp;</u></em></strong></h1>
+                    <p class="mbr-text mbr-fonts-style display-7">
+                        <br>The delicious, all-natural blend of rare fruit and plant extracts that allows you to burn off stubborn fat without any extra effort... Allowing you to finally have the good health, toned and tight body you deserve.<br><br>Melt away Your Stubborn Fat With This Secret Formula &amp; Flip Back Your Metabolic Switch within minutes For Burning Fat like Crazy.<br><br><strong><em>Limited Time Special Pricing - Act Now!<br>Secure Your Reserved <u>Ikaria Juice</u></em></strong><u>™️</u><strong><em>&nbsp;While Stocks Last</em></strong><br></p>
+                    <div class="mbr-section-btn mt-3"><a class="btn btn-info display-7" href="https://cutt.ly/officialikariajuice">Visit The Official Website</a></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section data-bs-version="5.1" class="info3 cid-tEoVqNnqwk" id="info3-r">
+
+    
+
+    
+    
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="card col-12 col-lg-10">
+                <div class="card-wrapper">
+                    <div class="card-box align-center">
+                        <h4 class="card-title mbr-fonts-style align-center mb-4 display-2"><strong>Limited Time Special Pricing - Act Now!</strong></h4>
+                        <p class="mbr-text mbr-fonts-style mb-4 display-5"><strong>!!</strong> Secure Your Reserved <u>Ikaria Lean Belly Juice™️</u> While Stocks Last <strong>!!</strong></p>
+                        <div class="mbr-section-btn mt-3"><a class="btn btn-info display-4" href="https://cutt.ly/officialikariajuice">&gt; Claim Your Massive Discount Now &lt;</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section data-bs-version="5.1" class="pricing2 cid-tDybsEKil6" id="pricing2-n">
+    
+
+    
+    
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-6 align-center col-lg-4">
+                <div class="plan">
+                    <div class="plan-header">
+                        <h6 class="plan-title mbr-fonts-style mb-3 display-5">
+                            <strong>1 BOTTLE</strong></h6>
+                        <div class="plan-price">
+                            <p class="price mbr-fonts-style m-0 display-1"><strong>$69</strong></p>
+                            <p class="price-term mbr-fonts-style mb-3 display-7"><strong>PER BOTTLE</strong></p>
+                        </div>
+                    </div>
+                    <div class="plan-body">
+                        <div class="plan-list mb-4">
+                            <ul class="list-group mbr-fonts-style list-group-flush display-7">
+                                <li class="list-group-item">1-Month Supply</li><li class="list-group-item">Before Price: $99</li><li class="list-group-item"><span style="background-color: transparent; font-size: 1.2rem;">Now: $79.00</span></li><li class="list-group-item"><span style="background-color: transparent; font-size: 1.2rem;">You save $30.00</span><br></li><li class="list-group-item">60-DAY GUARANTEE</li><li class="list-group-item">+ FREE SHIPPING</li>
+                            </ul>
+                        </div>
+                        <div class="mbr-section-btn text-center"><a href="https://cutt.ly/officialikariajuice" class="btn btn-success display-4">ADD TO CART</a></div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 align-center col-lg-4">
+                <div class="plan">
+                    <div class="plan-header">
+                        <h6 class="plan-title mbr-fonts-style mb-3 display-5">
+                            <strong>6 BOTTLE</strong></h6>
+                        <div class="plan-price">
+                            <p class="price mbr-fonts-style m-0 display-1"><strong>$49</strong></p>
+                            <p class="price-term mbr-fonts-style mb-3 display-7"><strong>PER BOTTLE</strong></p>
+                        </div>
+                    </div>
+                    <div class="plan-body">
+                        <div class="plan-list mb-4">
+                            <ul class="list-group mbr-fonts-style list-group-flush display-7">
+                                <li class="list-group-item">6-Month Supply + Bonus</li><li class="list-group-item">Before Price: $594</li><li class="list-group-item">Now: $294.00</li><li class="list-group-item">You save $300.00</li><li class="list-group-item">60-DAY GUARANTEE</li><li class="list-group-item">+ FREE SHIPPING</li>
+                            </ul>
+                        </div>
+                        <div class="mbr-section-btn text-center"><a href="https://cutt.ly/officialikariajuice" class="btn btn-secondary display-4">ADD TO CART</a></div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 align-center col-lg-4">
+                <div class="plan">
+                    <div class="plan-header">
+                        <h6 class="plan-title mbr-fonts-style mb-3 display-5">
+                            <strong>3 BOTTLE</strong></h6>
+                        <div class="plan-price">
+                            <p class="price mbr-fonts-style m-0 display-1"><strong>$59</strong></p>
+                            <p class="price-term mbr-fonts-style mb-3 display-7"><strong>PER BOTTLE</strong></p>
+                        </div>
+                    </div>
+                    <div class="plan-body">
+                        <div class="plan-list mb-4">
+                            <ul class="list-group mbr-fonts-style list-group-flush display-7">
+                                <li class="list-group-item">3-Month Supply + Bonus</li><li class="list-group-item">Before Price: $297</li><li class="list-group-item">Now: $177</li><li class="list-group-item">You save $120.00</li><li class="list-group-item">60-DAY GUARANTEE</li><li class="list-group-item">+ FREE SHIPPING</li>
+                            </ul>
+                        </div>
+                        <div class="mbr-section-btn text-center"><a href="https://cutt.ly/officialikariajuice" class="btn btn-success display-4">ADD TO CART</a></div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</section>
+
+<section data-bs-version="5.1" class="footer3 cid-s48P1Icc8J" once="footers" id="footer3-i">
+
+    
+
+    
+
+    <div class="container">
+        <div class="media-container-row align-center mbr-white">
+            <div class="row row-links">
+                <ul class="foot-menu">
+                    
+                    
+                    
+                    
+                    
+                <li class="foot-menu-item mbr-fonts-style display-7"><a href="" class="text-warning text-primary">Terms &amp; Conditions</a></li><li class="foot-menu-item mbr-fonts-style display-7"><a href="" class="text-warning text-primary">Privacy</a></li><li class="foot-menu-item mbr-fonts-style display-7"><a href="" class="text-warning text-primary">Disclaimer&nbsp;</a></li></ul>
+            </div>
+            
+            <div class="row row-copirayt">
+                <p class="mbr-text mb-0 mbr-fonts-style mbr-white align-center display-7">&nbsp;</p><p>COPYRIGHT© IKARIA LEAN BELLY JUICE - ALL RIGHTS RESERVED.</p><p>&nbsp;</p>
+            </div>
+        </div>
+    </div>
+  <script src="assets/smoothscroll/smooth-scroll.js"></script>  <script src="assets/ytplayer/index.js"></script>  <script src="assets/theme/js/script.js"></script>  
+  
+  
+</body>
+</html>
+';
+?>
